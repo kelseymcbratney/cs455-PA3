4857e7c994552330da87859b77e0d3ecd3af19ff
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-07 17:03:08 -0600

    sh script

--- build.sh
+++ build.sh
@@ -6,13 +6,6 @@ gradle clean
 # Build the Gradle project
 gradle build
 
-# Check if the build was successful
-if [ $? -eq 0 ]; then
-    echo "Build successful."
-    # Copy the JAR file to the Hadoop directory
-    cp build/libs/JobRunner.jar "$HADOOP_HOME"
-    echo "Copied JAR to main directory."
-else
-    echo "Build failed. Please check your code and try again."
-    exit 1
-fi
+# Copy the JAR file to the root directory
+cp build/libs/JobRunner.jar .
+
