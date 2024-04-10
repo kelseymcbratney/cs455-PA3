cbf66f518de8805164dcf3b3703b20cc71d21945
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-10 01:34:04 -0600

    sorted

--- src/main/java/csx55/hadoop/jobs/songCount/SongCountReducer.java
+++ src/main/java/csx55/hadoop/jobs/songCount/SongCountReducer.java
@@ -5,9 +5,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class SongCountReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
-
-    private final LongWritable result = new LongWritable();
+public class SongCountReducer extends Reducer<Text, LongWritable, LongWritable, Text> {
 
     @Override
     protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
@@ -16,8 +14,7 @@ public class SongCountReducer extends Reducer<Text, LongWritable, Text, LongWrit
         for (LongWritable value : values) {
             sum += value.get();
         }
-        result.set(sum);
-        // Output the artist with the highest count
-        context.write(key, result);
+        // Output (count, artist_name) pairs
+        context.write(new LongWritable(sum), key);
     }
 }
