1e93b4fdbee4cffb9357d24158a72d9bbbd27a4f
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-10 16:05:05 -0600

    fix

--- src/main/java/csx55/hadoop/jobs/songCount/SongCountReducer.java
+++ src/main/java/csx55/hadoop/jobs/songCount/SongCountReducer.java
@@ -5,19 +5,23 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
-public class SongCountReducer extends Reducer<Text, Text, LongWritable, Text> {
+public class SongCountReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         long sum = 0;
         // Iterate through all the values for a particular key
+        String[] parts = null;
+        int artistIndex = 0;
+        int countIndex = 0;
         for (Text value : values) {
-            String[] parts = value.toString().split(",");
+            parts = value.toString().split(",");
             // Assuming the count is at the last index
-            int countIndex = parts.length - 1;
+            countIndex = parts.length - 1;
+            artistIndex = parts.length - 3;
             sum += Long.parseLong(parts[countIndex]);
         }
         // Write the sum as the key and the original key as the value
-        context.write(new LongWritable(sum), key);
+        context.write(key,new Text(parts[artistIndex] + "," + sum));
     }
 }
