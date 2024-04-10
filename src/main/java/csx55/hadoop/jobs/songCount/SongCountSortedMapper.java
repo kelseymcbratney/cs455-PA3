6217867e2b10f827b2919f1728b5aeb3e67f7b44
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-10 15:12:28 -0600

    test

--- src/main/java/csx55/hadoop/jobs/songCount/SongCountSortedMapper.java
+++ src/main/java/csx55/hadoop/jobs/songCount/SongCountSortedMapper.java
@@ -5,16 +5,16 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class SongCountSortedMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
-
-    @Override
-    protected void map(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        long sum = 0;
-        // Iterate through all the values for a particular key
-        for (Text value : values) {
-            sum += Long.parseLong(value.toString());
-        }
-        // Write the sum as the key and the original key as the value
-        context.write(new LongWritable(sum), key);
-    }
-}
+//public class SongCountSortedMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+//
+//    @Override
+//    protected void map(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+//        long sum = 0;
+//        // Iterate through all the values for a particular key
+//        for (Text value : values) {
+//            sum += Long.parseLong(value.toString());
+//        }
+//        // Write the sum as the key and the original key as the value
+//        context.write(new LongWritable(sum), key);
+//    }
+//}
