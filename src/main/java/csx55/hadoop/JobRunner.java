bba2b35f9edd00a5794166fbd7ef8ecc848f63b0
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-10 17:04:06 -0600

    sorted

--- src/main/java/csx55/hadoop/JobRunner.java
+++ src/main/java/csx55/hadoop/JobRunner.java
@@ -23,12 +23,14 @@ public class JobRunner {
 
         switch (jobType) {
             case "0":
-                runSongCountJob(args);
-                runSortedSongCountJob(args);
+                if (!runSongCountJob(args)) {
+                    System.exit(1);
+                }
                 break;
             case "1":
-                runSongCountJob(args);
-                runSortedSongCountJob(args);
+                if (!runSongCountJob(args)) {
+                    System.exit(1);
+                }
                 break;
             default:
                 System.err.println("Invalid job type. Available options:\n1. Run All Jobs, \n2. SongCount");
@@ -36,7 +38,7 @@ public class JobRunner {
         }
     }
 
-    private static void runSongCountJob(String[] args) throws Exception {
+    private static boolean runSongCountJob(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf);
         job.setJarByClass(JobRunner.class);
@@ -52,30 +54,35 @@ public class JobRunner {
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
         boolean success = job.waitForCompletion(true);
-        System.exit(success ? 0 : 1);
+
+        if (success) {
+            Job sortJob = Job.getInstance();
+            sortJob.setJarByClass(JobRunner.class); // Ensure JobRunner is your main class
+
+            // Set the new classes for the mapper and reducer
+            sortJob.setMapperClass(SongCountSortedMapper.class);
+            sortJob.setReducerClass(SongCountSortedReducer.class);
+
+            // Setting the number of reduce tasks
+            sortJob.setNumReduceTasks(1); // Only one reducer to ensure global ordering
+
+            // If you want to sort in descending order, use the DecreasingComparator as before
+            sortJob.setSortComparatorClass(LongWritable.DecreasingComparator.class);
+
+            // Set the map output key/value classes according to the mapper's outputs
+            sortJob.setMapOutputKeyClass(LongWritable.class);
+            sortJob.setMapOutputValueClass(Text.class);
+
+            // Set the final output key/value classes according to the reducer's outputs
+            sortJob.setOutputKeyClass(Text.class);
+            sortJob.setOutputValueClass(LongWritable.class);
+
+            FileInputFormat.addInputPath(sortJob, new Path(args[2]));
+            FileOutputFormat.setOutputPath(sortJob, new Path(args[2] + "_sorted"));
+
+        }
+
+        return job.waitForCompletion(true);
     }
 
-    private static void runSortedSongCountJob(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf);
-        job.setJarByClass(JobRunner.class);
-
-        job.setMapperClass(SongCountSortedMapper.class);
-        job.setReducerClass(SongCountSortedReducer.class);
-
-        job.setNumReduceTasks(1);
-        job.setSortComparatorClass(LongWritable.DecreasingComparator.class);
-
-        job.setMapOutputKeyClass(LongWritable.class);
-        job.setMapOutputValueClass(Text.class);
-
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(LongWritable.class);
-
-        FileInputFormat.setInputPaths(job, new Path(args[2]));
-        FileOutputFormat.setOutputPath(job, new Path(args[2] + "_sorted"));
-
-        boolean success = job.waitForCompletion(true);
-        System.exit(success ? 0 : 1);
-    }
 }
