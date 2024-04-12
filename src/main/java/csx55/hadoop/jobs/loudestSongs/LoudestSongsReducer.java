1ddef37fb9734b7d90115604c2c689260f863d62
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-11 23:55:25 -0600

    side reduce

--- src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestSongsReducer.java
+++ src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestSongsReducer.java
@@ -11,30 +11,31 @@ public class LoudestSongsReducer extends Reducer<Text, Text, Text, Text> {
         String songName = null;
         String loudness = null;
         String artistName = null;
+        String type = null;
+        String data = null;
 
         for (Text value : values) {
             String[] parts = value.toString().split("\\|");
-            String type = parts[0];
-            String data = parts[1];
+            type = parts[0];
+            data = parts[1];
 
-            if (type.equals("A")) {
-                if (!data.equals("nan")) { // Filter out NaN values
-                    loudness = data;
-                }
-            } else if (type.equals("B")) {
-                int commaIndex = data.indexOf(',');
-                if (commaIndex != -1) {
-                    songName = data.substring(0, commaIndex);
-                    artistName = data.substring(commaIndex + 1);
-                } else {
-                    songName = data; // Assuming entire data is songName if no comma is present
-                    artistName = "Unknown"; // Default or null, depending on requirement
-                }
-            }
+//            if (type.equals("A")) {
+//                if (!data.equals("nan")) { // Filter out NaN values
+//                    loudness = data;
+//                }
+//            } else if (type.equals("B")) {
+//                int commaIndex = data.indexOf(',');
+//                if (commaIndex != -1) {
+//                    songName = data.substring(0, commaIndex);
+//                    artistName = data.substring(commaIndex + 1);
+//                } else {
+//                    songName = data; // Assuming entire data is songName if no comma is present
+//                    artistName = "Unknown"; // Default or null, depending on requirement
+//                }
+//            }
+
         }
 
-        if (songName != null && loudness != null) {
-            context.write(key, new Text(songName + "," + loudness + "," + artistName));
-        }
+        context.write(new Text(songName), new Text(type + " - " + data));
     }
 }
