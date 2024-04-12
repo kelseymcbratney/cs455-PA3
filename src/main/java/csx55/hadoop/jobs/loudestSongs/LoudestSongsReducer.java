c3ff24e879ed5f49cefb5b324a35862ec5a79a61
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-11 23:40:10 -0600

    side reduce

--- src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestSongsReducer.java
+++ src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestSongsReducer.java
@@ -8,26 +8,30 @@ public class LoudestSongsReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        String songName = null;
-        String loudness = null;
-        String artistName = null;
-
+//        String songName = null;
+//        String loudness = null;
+//        String artistName = null;
+//
+//        for (Text value : values) {
+//            String[] parts = value.toString().split("\\|");
+//            String type = parts[0];
+//            String data = parts[1];
+//
+//            if (type.equals("A")) {
+//                loudness = data;
+//            } else if (type.equals("B")) {
+//                String[] songInfo = data.split(",");
+//                songName = songInfo[0];
+//                artistName = songInfo[1];
+//            }
+//        }
+//
+//        if (songName != null && loudness != null) {
+//            context.write(key, new Text(songName + "," + loudness + "," + artistName));
+//        }
+        // output key value as is
         for (Text value : values) {
-            String[] parts = value.toString().split("\\|");
-            String type = parts[0];
-            String data = parts[1];
-
-            if (type.equals("A")) {
-                loudness = data;
-            } else if (type.equals("B")) {
-                String[] songInfo = data.split(",");
-                songName = songInfo[0];
-                artistName = songInfo[1];
-            }
-        }
-
-        if (songName != null && loudness != null) {
-            context.write(key, new Text(songName + "," + loudness + "," + artistName));
+            context.write(key, value);
         }
     }
 }
