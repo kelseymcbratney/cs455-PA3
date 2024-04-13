61b8b17f61748b0a39aeb9a1ab41deb92d3a4fe1
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-13 16:12:43 -0600

    test

--- src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistMapper.java
+++ src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistMapper.java
@@ -12,7 +12,8 @@ public class LoudestAverageArtistMapper extends Mapper<LongWritable, Text, Text,
         String[] parts = value.toString().split(", ");
         if (parts.length >= 3) { // Ensure there are enough parts to avoid ArrayIndexOutOfBoundsException
             try {
-                String artistID = parts[1].trim();
+                String artistID = parts[0].trim();
+                System.out.println("Artist ID: " + artistID);
                 double loudness = Double.parseDouble(parts[2]); // Correctly parse the loudness as double
                 context.write(new Text(artistID), new DoubleWritable(loudness));
             } catch (NumberFormatException e) {
