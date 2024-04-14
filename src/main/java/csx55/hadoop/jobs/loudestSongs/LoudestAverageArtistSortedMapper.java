638c68f04b809acfc8165d7bb5018e0efc0694f6
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-14 10:28:00 -0600

    test

--- src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistSortedMapper.java
+++ src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistSortedMapper.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class LoudestAverageArtistSortedMapper extends Mapper<LongWritable, Text, DoubleWritable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] parts = value.toString().split("\t");
+        String[] parts = value.toString().split(", ");
         double loudness = Double.parseDouble(parts[1]);  // Assuming loudness is the second part
         String artistName = parts[0];
         context.write(new DoubleWritable(-loudness), new Text(artistName + ", " + loudness));
