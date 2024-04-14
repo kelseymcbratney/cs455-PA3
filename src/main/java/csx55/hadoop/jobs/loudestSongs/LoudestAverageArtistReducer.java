592f54b8be2566813f0dad5cecba73e04cf0a702
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-14 14:39:44 -0600

    revert new baseline

--- src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistReducer.java
+++ src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistReducer.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.*;
 
 import java.io.IOException;
 
-public class LoudestAverageArtistReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+public class LoudestAverageArtistReducer extends Reducer<Text, DoubleWritable, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
@@ -16,6 +16,6 @@ public class LoudestAverageArtistReducer extends Reducer<Text, DoubleWritable, T
             count++;
         }
         double average = count > 0 ? sum / count : 0;
-        context.write(key, new DoubleWritable(average));
+        context.write(key, new Text(String.valueOf(average)));
     }
 }
