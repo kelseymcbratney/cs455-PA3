6e1c8d54fc7df6615b6cc170f9f93c32ee9683b6
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-14 10:38:33 -0600

    test

--- src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistReducer.java
+++ src/main/java/csx55/hadoop/jobs/loudestSongs/LoudestAverageArtistReducer.java
@@ -5,17 +5,19 @@ import org.apache.hadoop.mapreduce.*;
 
 import java.io.IOException;
 
-public class LoudestAverageArtistReducer extends Reducer<Text, Text, Text, DoubleWritable> {
+public class LoudestAverageArtistReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        String artistName = "";
         double sum = 0;
         int count = 0;
         for (Text val : values) {
             sum += Double.parseDouble(val.toString().split(", ")[1]);
             count++;
+            artistName = val.toString().split(", ")[0];
         }
         double average = count > 0 ? sum / count : 0;
-        context.write(key, new DoubleWritable(average));
+        context.write(key, new Text(artistName + average));
     }
 }
