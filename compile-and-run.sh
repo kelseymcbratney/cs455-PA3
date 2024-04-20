641295d305ee10bed2eee6aa0c4a2449a3d9a512
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-04-20 00:54:30 -0600

    fadeInFixed

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -11,7 +11,7 @@ hadoop fs -rm -r /PA3/output_topHotttnesss
 hadoop fs -rm -r /PA3/output_topHotttnesssSorted
 hadoop fs -rm -r /PA3/output_topFadeIn
 hadoop fs -rm -r /PA3/output_topFadeInSorted
-
+hadoop fs -rm -r /PA3/output_topFadeInOrder
 
 rm JobRunner.jar
 
