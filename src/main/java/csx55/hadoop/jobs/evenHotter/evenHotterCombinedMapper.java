0c893bb601d174ee6457cb78b4734f041fb83e7e
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-05-06 00:11:00 -0600

    even hotter

--- src/main/java/csx55/hadoop/jobs/evenHotter/evenHotterCombinedMapper.java
+++ src/main/java/csx55/hadoop/jobs/evenHotter/evenHotterCombinedMapper.java
@@ -12,7 +12,7 @@ public class evenHotterCombinedMapper extends Mapper<LongWritable, Text, DoubleW
         // Input from previous Reducer: key = songId, value = artistName|songTitle|hotttnesss
         String[] parts = value.toString().split("\\|");
         // (artistName + "|" + songTitle + "|" + hotttnesss + "|" + tempo + "|" + timeSignature + "|" + keySignature + "|" + energy + "|" + danceability + "|" + duration + "|" + loudness));
-            if (parts.length == 10) {  // Ensure that there are exactly ten parts: artistName, songTitle, hotttnesss, tempo, timeSignature, keySignature, energy, danceability, duration, loudness
+            if (parts.length == 9) {  // Ensure that there are exactly ten parts: artistName, songTitle, hotttnesss, tempo, timeSignature, keySignature, energy, danceability, duration, loudness
             String songTitle = parts[0];
             String artistName = parts[0];
             double hotttnesss;
