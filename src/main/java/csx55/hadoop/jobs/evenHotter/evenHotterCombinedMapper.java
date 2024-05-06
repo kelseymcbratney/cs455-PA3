4b9112a8344c8676d86612b623262023ce125a63
Kelsey McBratney <kelsey.mcbratney@gmail.com>
2024-05-06 00:06:43 -0600

    even hotter

--- src/main/java/csx55/hadoop/jobs/evenHotter/evenHotterCombinedMapper.java
+++ src/main/java/csx55/hadoop/jobs/evenHotter/evenHotterCombinedMapper.java
@@ -13,15 +13,15 @@ public class evenHotterCombinedMapper extends Mapper<LongWritable, Text, DoubleW
         String[] parts = value.toString().split("\\|");
         // (artistName + "|" + songTitle + "|" + hotttnesss + "|" + tempo + "|" + timeSignature + "|" + keySignature + "|" + energy + "|" + danceability + "|" + duration + "|" + loudness));
             if (parts.length == 10) {  // Ensure that there are exactly ten parts: artistName, songTitle, hotttnesss, tempo, timeSignature, keySignature, energy, danceability, duration, loudness
+            String songTitle = parts[0];
             String artistName = parts[0];
-            String songTitle = parts[1];
             double hotttnesss;
             try {
-                hotttnesss = Double.parseDouble(parts[2]);
+                hotttnesss = Double.parseDouble(parts[1]);
                 // Emit negative to sort in descending order
                 context.write(new DoubleWritable(-hotttnesss), new Text(artistName + "|" + songTitle + "|" + parts[3] + "|" + parts[4] + "|" + parts[5] + "|" + parts[6] + "|" + parts[7] + "|" + parts[8] + "|" + parts[9]));
             } catch (NumberFormatException e) {
-                System.err.println("Error parsing hotttnesss: " + parts[2]);
+                System.err.println("Error parsing hotttnesss: " + parts[1]);
             }
         }
     }
